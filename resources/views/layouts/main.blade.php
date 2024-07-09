78472816b4623d63230666687e0b201e7bc0b745
Mahmut İÇME <arrovain@gmail.com>
2024-07-09 16:31:20 +0300

    main

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tercan Sigorta</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    @include('partials.navbar')
+    @yield('content')
+    @include('partials.footer')
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>

004850a64911d007d1ccac0e49aacbe7fc324652
Mahmut İÇME <arrovain@gmail.com>
2024-07-09 16:32:57 +0300

    partials/navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Tercan Sigorta</a>
+    <div class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item"><a class="nav-link" href="/">Anasayfa</a></li>
+            <li class="nav-item"><a class="nav-link" href="/kurumsal">Kurumsal</a></li>
+            <li class="nav-item"><a class="nav-link" href="/urunler">Ürünler</a></li>
+            <li class="nav-item"><a class="nav-link" href="/iletisim">İletişim</a></li>
+        </ul>
+    </div>
+</nav>

8187178ffb08e96c2564ea52c23d7fdbbfc06c87
Mahmut İÇME <arrovain@gmail.com>
2024-07-09 15:47:03 +0300

    bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hercan</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
 </head>
 <body>
     <header>

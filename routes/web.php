8377a504e9cdf61cd42ee60ea8b8af95ce339a6b
Mahmut İÇME <arrovain@gmail.com>
2024-07-09 17:13:13 +0300

    products protected fillable

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
 Route::get('/admin', function () {
     return view('admin');
-})->middleware('auth');
-
+});

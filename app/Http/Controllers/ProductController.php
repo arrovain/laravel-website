aac6002c045dfea355cb40ed7a62944c869486da
Mahmut İÇME <arrovain@gmail.com>
2024-07-09 16:08:59 +0300

    store function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function store(Request $request)
+    { 
+        $product = new Product;
+        $product->name = $request->name;
+        
+        $product->save();
+        
+        return response()->json(['success' => 'Ürün başarıyla eklendi']);
+    }
 }

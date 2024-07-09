fb7fbaea216070547cf8c11d505697935f8da6d2
Mahmut İÇME <arrovain@gmail.com>
2024-07-09 16:41:12 +0300

    footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,30 @@
+<footer class="bg-light text-center text-lg-start">
+    <div class="container p-4">
+        <div class="row">
+            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Tercan Sigorta</h5>
+                <p>2014 yılında kurulan şirketimiz Türkiye Genelinde 100'den fazla şubesi ve +25 acenteliği ile 10 yıldır sigortacılık sektöründe hizmet vermeye devam etmektedir.</p>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Menü</h5>
+                <ul class="list-unstyled mb-0">
+                    <li><a href="/" class="text-dark">Anasayfa</a></li>
+                    <li><a href="/kurumsal" class="text-dark">Kurumsal</a></li>
+                    <li><a href="/urunler" class="text-dark">Ürünler</a></li>
+                    <li><a href="/iletisim" class="text-dark">İletişim</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">İletişim</h5>
+                <ul class="list-unstyled mb-0">
+                    <li>Gevher Nesibe Mah.Tuğcu Sok Molu İş Merkezi No 21 38015 Kocasinan/KAYSERİ</li>
+                    <li>Email: info@hercan.com.tr</li>
+                    <li>Telefon: +90 (352) 231 43 43</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="text-center p-3 bg-dark text-white">
+        © 2021 Tercan Sigorta | Aracılık Hizmetleri
+    </div>
+</footer>

32a9ceafcd2fb27a7bd53bf770db4397a58b6ad4
Mahmut İÇME <arrovain@gmail.com>
2024-07-10 17:24:12 +0300

    news manager

---
+++ resources/views/admin/news.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Haber Yönetimi</h1>
+        <div id="app">
+            <news-manager></news-manager>
+        </div>
+    </div>
+@endsection

5e7476e9c6fa20e3783673e1213233dcdc6f5c57
Mahmut İÇME <arrovain@gmail.com>
2024-07-10 17:23:02 +0300

    product manager

---
+++ resources/views/admin/products.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Ürün Yönetimi</h1>
+        <div id="app">
+            <product-manager></product-manager>
+        </div>
+    </div>
+@endsection

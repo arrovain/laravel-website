424c45416478b397c9955b51545d4e9b930666bc
Mahmut İÇME <arrovain@gmail.com>
2024-07-10 17:34:04 +0300

    prefix for admin

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,19 @@ Route::get('/', function () {
 });
 
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/news', [NewsController::class, 'index'])->name('news.index');
+Route::get('/news', [NewsController::class, 'index'])->name('news.index');(
+
+Route::prefix('admin')->middleware(['auth'])->group(function () {
+    
+})
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
